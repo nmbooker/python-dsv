d66f95763e1159903e1f922eba87d52466f54dd5
Nick Booker <NMBooker@gmail.com>
2014-02-19 16:55:47 +0000

    Add a basic index of scripts to the top-level dsv module.

--- dsv/__init__.py
+++ dsv/__init__.py
@@ -3,6 +3,16 @@
 """Contains the DSV dialect for Python's CSV module
 
 Should also be compatible with the oekit.unicsv module too.
+
+
+There are two scripts provided too (for more info run with --help or read README.mkd):
+
+ dsv2csv     --  takes DSV colon-separated data on stdin and outputs
+                 CSV on stdout in the csv module's default dialect 'excel'
+
+ dsvjoinline --  takes a series of positional arguments, and outputs those
+                 arguments joined together into a single DSV record on stdout.
+                 stdin is not read.
 """
 
 import csv
